9b102aac64cbc00bc95111e47a4ea0d78902d83f
sparker <scott.p.parker@gmail.com>
2021-07-31 10:17:33 -0500

    Clean up the RWX logo

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 nativefier --icon android-messages-logo.png --name "Google Messages" https://messages.google.com/web --honest --disable-dev-tools --single-instance _builds
-nativefier --icon rwx-logo.jpg --name "ReadWriteExecute" https://readwriteexecute.slack.com --honest --disable-dev-tools --single-instance _builds
-mv _builds/*/*.app /Applications
+nativefier --icon rwx-logo.png --name "ReadWriteExecute" https://readwriteexecute.slack.com --honest --disable-dev-tools --single-instance _builds
+for app in _builds/*/*.app; do
+  rm -rf "/Applications/`basename "$app"`"
+    # do some stuff here with "$f"
+    # remember to quote it or spaces may misbehave
+done
+mv -f _builds/*/*.app /Applications
